f9e46eb3960a0cc421d5f155c6870c57ffdb697a
Jonas Bernoulli <jonas@bernoul.li>
2010-08-13 12:18:19 +0200

    support for toggling column visibility

--- listing.el
+++ listing.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Created: 20100605
-;; Updated: 20100812
+;; Updated: 20100813
 ;; Version: 0.1.3+
 ;; Homepage: https://github.com/tarsius/listing
 ;; Keywords: convenience
@@ -52,7 +52,7 @@
   (setq truncate-lines t
 	buffer-read-only t
 	x-stretch-cursor nil
-	buffer-invisibility-spec nil))
+	buffer-invisibility-spec '(t)))
 
 (defun listing-create (value buffer-or-name columns &optional column mode)
   "Insert elements of the list VALUE into BUFFER, one per line.
@@ -64,7 +64,7 @@ COLUMN is the column by which the elements are sorted initialy.  COLUMNS
 specifies what parts are inserted and can optionally be used to format
 the output of each column individually. It has the form:
 
-  ((HEADER LENGTH ACCESSOR)...)
+  ((HEADER LENGTH VISIBLE ACCESSOR)...)
 
 HEADER is a string used as label in the header line for the respective
 column.  ACCESSOR is a function used to extract the value for the
@@ -76,7 +76,13 @@ to be inserted.  LENGTH defined the minimal length of the column."
 	  (inhibit-point-motion-hooks t))
       (erase-buffer)
       (funcall (or mode 'listing-mode))
-      (setq listing-buffer-columns columns)
+      (setq listing-buffer-columns
+	    (mapcar (lambda (column)
+		      (unless (caddr column)
+			(add-to-invisibility-spec
+			 (listing-column-symbol column)))
+		      (copy-list column))
+		    columns))
       (listing-insert value)
       (listing-sort column)
       (set-buffer-modified-p nil)
@@ -133,6 +139,30 @@ to be inserted.  LENGTH defined the minimal length of the column."
 This allows all listing elements to be seen."
   (interactive)
   (setq buffer-invisibility-spec nil))
+
+(defun listing-hide-column (column)
+  (interactive
+   (list (completing-read "Column: "
+			  (mapcan (lambda (col)
+				    (when (caddr col)
+				      (list (car col))))
+				  listing-buffer-columns)
+			  nil t)))
+  (add-to-invisibility-spec (listing-column-symbol column))
+  (setf (caddr (assoc column listing-buffer-columns)) nil)
+  (listing-align))
+
+(defun listing-show-column (column)
+  (interactive
+   (list (completing-read "Column: "
+			  (mapcan (lambda (col)
+				    (unless (caddr col)
+				      (list (car col))))
+				  listing-buffer-columns)
+			  nil t)))
+  (remove-from-invisibility-spec (listing-column-symbol column))
+  (setf (caddr (assoc column listing-buffer-columns)) t)
+  (listing-align))
 
 ;;; List Functions.
 
@@ -208,63 +238,73 @@ This allows all listing elements to be seen."
    (lambda (props start end)
      (funcall function (plist-get props :listing-element)))))
 
-;;; Element Functions.
+;;; Row Functions.
 
 (defun listing-format-element (elt)
-  (propertize
-   (concat (mapconcat
-	    (lambda (col)
-	      (let* ((face (when listing-element-font-function
-			     (funcall listing-element-font-function elt)))
-		     (value (funcall (caddr col) elt))
-		     (string (if (stringp value)
-				 (copy-sequence value)
-			       (prin1-to-string value))))
-		(when face
-		  (dotimes (i (length string))
-		    (unless (get-char-property i 'face string)
-		      (put-text-property i (1+ i) 'face face string))))
-		string))
-	    listing-buffer-columns "\037")
-	   "\n")
-   :listing-element elt
-   'point-entered 'listing-line-entered))
+  (let (text (columns listing-buffer-columns))
+    (while columns
+      (let* ((column (pop columns))
+	     (face   (when listing-element-font-function
+		       (funcall listing-element-font-function elt)))
+	     (value  (funcall (nth 3 column) elt))
+	     (string (if (stringp value)
+			 (copy-sequence value)
+		       (prin1-to-string value))))
+	(when face
+	  (dotimes (i (length string))
+	    (unless (get-char-property i 'face string)
+	      (put-text-property i (1+ i) 'face face string))))
+	(setq text
+	      (concat text
+		      (propertize (concat string (when columns "\037"))
+				  'invisible
+				  (list (listing-column-symbol column)))
+		      (unless columns "\n")))))
+  (propertize text
+	      :listing-element elt
+	      'point-entered 'listing-line-entered)))
 
 (defun listing-format-header ()
-  (listing-align-element
-   (concat " "
-	   (mapconcat (lambda (col)
-			(format-header-button
-			 (car col) :type 'listing-header
-			 'help-echo (concat "mouse-1: Sort by "
-					    (downcase (car col)))))
-		      listing-buffer-columns "\037"))))
+  (let (text (columns listing-buffer-columns))
+    (while columns
+      (let* ((column (pop columns))
+	     (header (car column)))
+	(when (caddr column)
+	  (setq text (concat text
+			     (format-header-button
+			      header :type 'listing-header
+			      'help-echo (concat "mouse-1: Sort by "
+						 (downcase header)))
+			     (when columns "\037"))))))
+    (listing-align-element (concat " " text))))
 
-(defun listing-align-element (&optional object)
+(defun listing-align-element (&optional header)
   (let ((columns listing-buffer-columns)
 	(regexp "\\([^\037\n]*\\)\\(\037\\)?")
 	(line-length 0)
 	(text-length 0)
-	(obj-length 1)) ; for header line!
+	(head-length 1))
     (while columns
-      (if (or (not object) (bufferp object))
-	  (re-search-forward regexp (line-end-position) t)
-	(string-match regexp object obj-length)
-	(incf obj-length (length (match-string 0 object))))
-      (let ((col (pop columns))
-	    (str (match-string 1 object)))
-	(incf line-length (cadr col))
-	(incf text-length (length str))
-	(when (match-string 2 object)
-	  (put-text-property
-	   (match-beginning 2) (match-end 2) 'display
-	   (list 'space :width
-		 (let ((space (1+ (max 0 (- line-length text-length)))))
-		   (incf text-length space)
-		   (incf line-length)
-		   space))
-	   object))))
-    object))
+      (let ((column (pop columns)))
+	(unless header
+	  (re-search-forward regexp (line-end-position) t))
+	(when (caddr column)
+	  (when header
+	    (string-match regexp header head-length)
+	    (incf head-length (length (match-string 0 header))))
+	  (let ((str (match-string 1 header)))
+	    (incf line-length (cadr column))
+	    (incf text-length (length str))
+	    (when (match-string 2 header)
+	      (put-text-property
+	       (match-beginning 2) (match-end 2) 'display
+	       (list 'space :width
+		     (let ((space (1+ (max 0 (- line-length text-length)))))
+		       (incf text-length space)
+		       (incf line-length)
+		       space))
+	       header))))))
+    header))
 
 (defun listing-line-entered (old new)
   (let ((old-elt (get-text-property old :listing-element))
@@ -290,6 +330,13 @@ This allows all listing elements to be seen."
 	;; Here we can't prevent the message from being shown twice.
 	(let ((message-log-max nil))
 	  (funcall listing-preview-element-function new-elt))))))
+
+;;; Utitlity Functions.
+
+(defun listing-column-symbol (column)
+  (intern (concat "column:" (downcase (if (listp column)
+					  (car column)
+					column)))))
 
 (provide 'listing)
 ;;; listing.el ends here
